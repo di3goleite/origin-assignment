79a31b5e0e9b59b8264b5f244a2dddb011b6b057
Diego Leite <diegojleite@gmail.com>
2022-01-25 23:06:52 -0300

    :white_check_mark: Refactor basic test for Card Component

--- frontend/src/components/Card/index.test.js
+++ frontend/src/components/Card/index.test.js
@@ -5,7 +5,7 @@ import Card from '.';
 
 describe('<Card />', () => {
   it('should render card', () => {
-    const { container } = render(<Card></Card>);
+    const { container } = render(<Card />);
 
     expect(container.firstChild).toHaveClass('card');
   });
