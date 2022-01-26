c74aa6d82796be3275efbe9f3990683eb688d38b
Diego Leite <diegojleite@gmail.com>
2022-01-26 19:41:29 -0300

    :sparkles: Export all components using a common file

---
+++ frontend/src/components/index.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import Card from './Card';
+import CurrencyInput from './CurrencyInput';
+import Navbar from './Navbar';
+import Scorebar from './Scorebar';
+
+export default {
+  Button,
+  Card,
+  CurrencyInput,
+  Navbar,
+  Scorebar
+};
