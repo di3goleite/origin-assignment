1bc7503e6bd63c55ae577cfd669f497f10123ee8
Diego Leite <diegojleite@gmail.com>
2022-01-27 00:22:43 -0300

    :sparkles: Add Result Container

--- frontend/src/containers/Result/index.js
+++ frontend/src/containers/Result/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 
 import components from '../../components';
+
+import icons from '../../assets/icons';
 import './index.scss';
 
-const { Navbar } = components;
+const { Navbar, Footer, Card, Button, Scorebar } = components;
+const scoreTypeMessage = {
+  healthy: 'Congratulations!',
+  average: 'There is room for improvement.',
+  unhealthy: 'Caution!'
+};
 
-function Result() {
+function Result({ score = 'healthy' }) {
+  const capitalize = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
   return (
     <div className="result">
       <Navbar />
       <div className="container">
-        <h1>Result</h1>
+        <h2>
+          Here&apos;s your <b>financial wellness score:</b>
+        </h2>
+        <Card>
+          <div className="card-header">
+            <img src={icons.originIcon} alt="Origin Financial Logo" />
+          </div>
+          <div className="card-body">
+            <Scorebar type={score} />
+            <div>
+              <h1>{scoreTypeMessage[score]}</h1>
+              <p>
+                {'Your financial wellness score is '}
+                <b>{capitalize(score)}</b>
+                {'.'}
+              </p>
+            </div>
+          </div>
+          <div className="card-footer">
+            <Button variant="secondary">Return</Button>
+          </div>
+        </Card>
       </div>
+      <Footer />
     </div>
   );
 }
