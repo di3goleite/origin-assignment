7d21558bfa3eeff71f396701110dbe21b4475b3f
Diego Leite <diegojleite@gmail.com>
2022-01-27 20:37:02 -0300

    :wrench: Configure environments for Backend application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const fastify = require('fastify')({
+  logger: {
+    level: 'info',
+    prettyPrint: process.env.APP_ENV === 'dev'
+  }
+});
+
+fastify.get('/', async () => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen(3001);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
