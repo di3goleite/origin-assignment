acb74353768d2c1ff5c7a6e101eade1a7dd54260
Diego Leite <diegojleite@gmail.com>
2022-01-28 00:15:23 -0300

    :memo: Setup documentation using Swagger

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,40 @@ const fastify = require('fastify')({
   }
 });
 
+fastify.register(require('fastify-swagger'), {
+  routePrefix: '/docs',
+  swagger: {
+    info: {
+      title: 'Origin Financial',
+      description: 'Financial wellness score calculation',
+      version: '0.1.0'
+    },
+    externalDocs: {
+      url: 'https://useorigin.notion.site/THA-Web-Interface-API-application-4819947101684706b984f04e9aef9294',
+      description: 'Find more info here'
+    },
+    host: `localhost:${APP_PORT}`,
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+  },
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    }
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  exposeRoute: true
+});
+
 // Routes
 fastify.post('/compute/score', financialLifeSchema, async (request) => {
   const { annualIncome, monthlyCosts } = request.body;
@@ -22,6 +56,7 @@ fastify.post('/compute/score', financialLifeSchema, async (request) => {
 const bootstrap = async () => {
   try {
     await fastify.listen(APP_PORT);
+    fastify.swagger();
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
