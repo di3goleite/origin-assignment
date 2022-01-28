271adfc01a1295456ce1c7a61df024939072af61
Diego Leite <diegojleite@gmail.com>
2022-01-28 18:41:40 -0300

    :bug: Avoid app breaks when there is no score on Result page

--- frontend/src/containers/Result/index.js
+++ frontend/src/containers/Result/index.js
@@ -13,36 +13,41 @@ const { Card, Button, Scorebar } = components;
 function Result() {
   const navigate = useNavigate();
   const location = useLocation();
-  const { score } = location.state;
 
-  return (
-    <BaseContainer className="result">
-      <h2>
-        Here&apos;s your <b>financial wellness score:</b>
-      </h2>
-      <Card>
-        <div className="card-header">
-          <img src={icons.origin} alt="Origin Financial Logo" />
-        </div>
-        <div className="card-body">
-          <Scorebar type={score} />
-          <div>
-            <h1>{scoreTypeMessage[score].title}</h1>
-            <p>
-              {'Your financial wellness score is '}
-              <b>{scoreTypeMessage[score].state}</b>
-              {'.'}
-            </p>
+  if (location.state) {
+    const { score } = location.state;
+
+    return (
+      <BaseContainer className="result">
+        <h2>
+          Here&apos;s your <b>financial wellness score:</b>
+        </h2>
+        <Card>
+          <div className="card-header">
+            <img src={icons.origin} alt="Origin Financial Logo" />
           </div>
-        </div>
-        <div className="card-footer">
-          <Button variant="secondary" onClick={() => navigate('/')}>
-            Return
-          </Button>
-        </div>
-      </Card>
-    </BaseContainer>
-  );
+          <div className="card-body">
+            <Scorebar type={score} />
+            <div>
+              <h1>{scoreTypeMessage[score].title}</h1>
+              <p>
+                {'Your financial wellness score is '}
+                <b>{scoreTypeMessage[score].state}</b>
+                {'.'}
+              </p>
+            </div>
+          </div>
+          <div className="card-footer">
+            <Button variant="secondary" onClick={() => navigate('/')}>
+              Return
+            </Button>
+          </div>
+        </Card>
+      </BaseContainer>
+    );
+  }
+
+  return null;
 }
 
 export default Result;
