25205e2e05e10f471ce59b1daf28851d4c1f1566
Diego Leite <diegojleite@gmail.com>
2022-01-28 21:51:03 -0300

    :memo: Add documentation for input utilities file

--- frontend/src/utils/input.js
+++ frontend/src/utils/input.js
@@ -1,3 +1,9 @@
+/**
+ * Check if a minimum constraint is valid
+ * @param {*} data input to be validated
+ * @param {*} min minimum accepted value
+ * @returns result of data validation
+ */
 const min = (data, min) => {
   if (data < min) {
     return {
@@ -8,6 +14,11 @@ const min = (data, min) => {
   return { success: true };
 };
 
+/**
+ * Check if a required constraint is valid
+ * @param {*} data input to be validated
+ * @returns result of data validation
+ */
 const required = (data) => {
   if (data === '') {
     return { success: false, message: `This field is required.` };
@@ -17,6 +28,12 @@ const required = (data) => {
 
 const validations = { min, required };
 
+/**
+ * Do input validation for a given data based on a requirements list
+ * @param {*} data input to be validated
+ * @param {*} requirements list of requirement objects describing type and condition to be checked against the data
+ * @returns list of found errors
+ */
 export const validation = (data, requirements) => {
   const errors = [];
 
