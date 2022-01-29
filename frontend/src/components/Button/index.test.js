c404b50eae2aa294a530d9964d30e5d05f4033c0
Diego Leite <diegojleite@gmail.com>
2022-01-28 21:59:43 -0300

    :white_check_mark: Add missing unity tests for Button Component

--- frontend/src/components/Button/index.test.js
+++ frontend/src/components/Button/index.test.js
@@ -9,4 +9,26 @@ describe('<Button />', () => {
     render(<Button>{buttonText}</Button>);
     expect(screen.getByText(buttonText)).toBeInTheDocument();
   });
+
+  it('should render a primary button', () => {
+    const buttonText = 'Primary';
+    const { container } = render(
+      <Button variant="primary">{buttonText}</Button>
+    );
+    expect(container.firstChild).toHaveClass('btn-primary');
+  });
+
+  it('should render a secondary button', () => {
+    const buttonText = 'Secondary';
+    const { container } = render(
+      <Button variant="secondary">{buttonText}</Button>
+    );
+    expect(container.firstChild).toHaveClass('btn-secondary');
+  });
+
+  it('should render a loading button', () => {
+    const buttonText = 'Loading...';
+    const { container } = render(<Button isLoading>{buttonText}</Button>);
+    expect(container.firstChild).toHaveClass('is-loading');
+  });
 });
