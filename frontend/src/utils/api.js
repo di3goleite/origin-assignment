0067c390567112d4de9e1aef220d88e275c5a62e
Diego Leite <diegojleite@gmail.com>
2022-01-28 21:53:49 -0300

    :memo: Add documentation for api utilities file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,11 @@ import config from './config';
 
 const headers = { 'Content-Type': 'application/json' };
 
+/**
+ * Compute user financial wellness score
+ * @param {*} payload financial information like { annualIncome, monthlyCosts }
+ * @returns JSON response like { score: HEALTHY }
+ */
 export const computeScore = async (payload) => {
   const response = await fetch(
     `${config.apiUrl}/${config.endpoints.computeScore}`,
